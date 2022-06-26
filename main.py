3f8689ceb5c51864dfc1ddf6f3bb6de9f999ab36
balal-art <balal.art@gmail.com>
2022-06-26 23:33:57 +0430

    ok

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ from plotly.subplots import make_subplots
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
 import plotly.express as px
-
-
-
 import numpy as np  
 import sys
 
